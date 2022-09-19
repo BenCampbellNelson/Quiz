c4db7f87dc366b4d673b130152c052367dae6335
BenCampbellNelson <campbellnelsonb@gmail.com>
2022-09-20 11:14:39 +1200

    start of quiz

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Quiz
 {
@@ -6,7 +7,57 @@ namespace Quiz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            string temp;
+            int score = 0;
+
+            do
+            {
+
+
+
+                Console.WriteLine("Welcome to this ture or false quiz");
+                Thread.Sleep(2000);
+
+
+
+                Console.WriteLine("Albert Einstein failed most of his subjects except for math and physics?");
+                temp = Console.ReadLine();
+                temp = temp.ToUpper();
+
+                    if (temp == "T")
+                {
+                    Console.WriteLine("Correct");
+                    Console.ReadLine();
+                    score += 1;
+
+                }
+                    else
+                {
+                    Console.WriteLine("incorrect");
+                     Console.ReadLine();
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                Console.WriteLine("Would you like to give the quiz another go? Y/N");
+                temp = Console.ReadLine(); 
+                temp = temp.ToUpper();
+
+            }while(temp == "Y")`;
+
         }
     }
 }
