2d6d0834934c26c64cd8ebfe867b0a282d320a5b
Reuben McArley (1000113227) <MCARRL1@student.op.ac.nz>
2022-09-20 11:21:24 +1200

    question 2 added

--- Program.cs
+++ Program.cs
@@ -12,33 +12,46 @@ namespace Quiz
 
             do
             {
-
-
-
-                Console.WriteLine("Welcome to this ture or false quiz");
+                Console.WriteLine("Welcome to this true or false quiz");
                 Thread.Sleep(2000);
 
-
-
+                //Question 1
                 Console.WriteLine("Albert Einstein failed most of his subjects except for math and physics?");
                 temp = Console.ReadLine();
                 temp = temp.ToUpper();
 
-                    if (temp == "T")
+                if (temp == "T")
                 {
                     Console.WriteLine("Correct");
                     Console.ReadLine();
-                    score += 1;
+                    score++;
 
                 }
-                    else
+                else
                 {
                     Console.WriteLine("incorrect");
-                     Console.ReadLine();
+                    Console.ReadLine();
                 }
 
+                //Question 2
+                Console.WriteLine("The first ever song sung in space was Happy Birthday?");
+                temp = Console.ReadLine();
+                temp = temp.ToUpper();
 
+                if (temp == "T")
+                {
+                    Console.WriteLine("Correct");
+                    Console.ReadLine();
+                    score++;
 
+                }
+                else
+                {
+                    Console.WriteLine("incorrect");
+                    Console.ReadLine();
+                }
+
+                //question 3
 
 
 
@@ -53,10 +66,10 @@ namespace Quiz
 
 
                 Console.WriteLine("Would you like to give the quiz another go? Y/N");
-                temp = Console.ReadLine(); 
+                temp = Console.ReadLine();
                 temp = temp.ToUpper();
 
-            }while(temp == "Y")`;
+            } while (temp == "Y");
 
         }
     }
